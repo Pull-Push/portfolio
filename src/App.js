3bf02b8e62730d6e7ada836a7eaf9f4b764ad43f
Jeff Sokol <JeffSokolDev@gmail.com>
2024-10-28 14:40:58 -0400

    landing page complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import QRCodeRender from './pages/FrontendMentor/QRCode';
+import Landing from './pages/Landing';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Landing />} />
         <Route path="/frontendmentor/qr" element={<QRCodeRender />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Landing.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../assets/styles/landingstyle.css'
+
+
+export default function Landing() {
+    return (
+        <div>
+            <div className="main mx-auto w-5/6 absolute inset-0 h-5/6">
+                <img className='min-w-52' id='logo' src={require("../assets/images/landing/logo2.png")} alt="logo" />
+                    <div className="links">
+                        <a href="https://github.com/Pull-Push">github</a>
+                        <a href="https://www.linkedin.com/in/jeffrey-sokol/">linkedin</a>
+                        <a href="/html/projects.html">projects</a>
+                        <a href="/docs/JSokol Resume.pdf" download>resume</a>
+                    </div>
+            </div>
+        </div>
+    )
+}
