e6139dca95852d0b5ee78e0379df569e9d2b845b
Jeff Sokol <JeffSokolDev@gmail.com>
2024-10-28 15:39:10 -0400

    quick and dirty resume

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import '../assets/styles/landingstyle.css'
-import { Link } from 'react-router-dom'
 
 
 export default function Landing() {
@@ -12,7 +11,7 @@ export default function Landing() {
                         <a href="https://github.com/Pull-Push">github</a>
                         <a href="https://www.linkedin.com/in/jeffrey-sokol/">linkedin</a>
                         {/* <a href="/html/projects.html">projects</a> */}
-                        <Link to="../../public/docs/JEFFREY_SOKOL_RESUME.pdf" target='_blank' download>resume</Link>
+                        <a href="https://jeffreyscottsokol.com/docs/JEFFREY_SOKOL_102024.pdf" download>resume</a>
                     </div>
             </div>
         </div>
