aed4c7670737731b89866a16a51720cd8fe00f80
Jeff Sokol <JeffSokolDev@gmail.com>
2024-10-31 10:56:16 -0400

    im dumb and forgot the word 'grid'

--- src/pages/FrontendMentor/QRCode.js
+++ src/pages/FrontendMentor/QRCode.js
@@ -4,7 +4,7 @@ import '../../assets/styles/qrstyle.css'
 
 export default function QRCodeRender(){
     return(
-        <div className='qr-component justify-items-center content-center'>
+        <div className='qr-component grid justify-items-center content-center'>
             <div className="bg-white p-3.5 rounded-2xl w-5/6 justify-items-center max-w-sm" >
                 <img src={require('../../assets/images/frontendmentor/image-qr-code.png')} alt='QR Code for Frontend Mentor' />
                 <h3 className='text-center pt-7 text-xl font-bold'>Improve your front-end skills by building projects </h3>
