6b73b9b2430c6c99c5da153bd17ed8aea6e649aa
Jeff Sokol <JeffSokolDev@gmail.com>
2024-11-01 18:47:36 -0400

    whoops forgot the hover state

--- src/pages/FrontendMentor/BlogPreview.js
+++ src/pages/FrontendMentor/BlogPreview.js
@@ -7,11 +7,11 @@ export default function BlogCardRender() {
             <div className="card bg-white p-5 rounded-2xl w-5/6 justify-items-center max-w-sm border-2 border-black min-w-min">
                 <BlogLogo />
                 <div className='justify-items-left'>
-                <div className='hover:bg-yellow-400 w-fit px-3.5'>
+                <div className='bg-yellow-400 w-fit px-3.5'>
                     <p className='text-black font-bold my-3.5'>Learning</p>
                 </div>
                 <p className='text-black text-sm font-bold mb-3.5'>Publshed 21 Dec 2023</p>
-                <p className='text-black text-xl font-extrabold mb-3.5'>HTML & CSS foundations</p>
+                <p className='hover:text-yellow-400 text-black text-xl font-extrabold mb-3.5'>HTML & CSS foundations</p>
                 <p className='mb-5'>These languages are the backbone of every website, defining structure, content, and presentation.</p>
                 <div className="flex justify-items-center content-end">
                     <img src={require("../../assets/images/frontendmentor/image-avatar.webp")} alt="" className="avatar w-1/12 mr-3.5" />
