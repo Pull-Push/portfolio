1ad51c32cbf229b8db4c9a3177e15ce3e53c9efc
Jeff Sokol <JeffSokolDev@gmail.com>
2024-11-04 21:01:34 -0500

    continuation of styling

--- src/pages/FrontendMentor/SocialLinks.js
+++ src/pages/FrontendMentor/SocialLinks.js
@@ -4,24 +4,23 @@ import '../../assets/styles/sociallink.css'
 
 export default function SocialLinkRender() {
     return (
-        <div className="social_link_component">
-            <div className="card text-center grid content-center justify-items-center">
-                <img src={require("../../assets/images/frontendmentor/avatar-jessica.jpeg")} alt="Jessica Randall" />
-                <h2>Jessica Randall</h2>
+        <div className="social_link_component grid justify-items-center content-center">
+            <div className="bg-[#1F1F1F] text-center grid content-center justify-items-center py-6 min-w-80">
+                <img className='w-24' src={require("../../assets/images/frontendmentor/avatar-jessica.jpeg")} alt="Jessica Randall" />
+                <h2 className="text-white">Jessica Randall</h2>
                 <h4 className="text-social-green">London, United Kingdom</h4>
-                <p>"Front-end developer and avid reader."</p>
-                <div className="links grid">
-                        <button href='#' className="text-center text-white hover:text-black hover:bg-social-green">GitHub</button>
-                        <button href='#' className="text-center text-white hover:text-black hover:bg-social-green">Frontend Mentor</button>
-                        <button href='#' className="text-center text-white hover:text-black hover:bg-social-green">LinkedIn</button>
-                        <button href='#' className="text-center text-white hover:text-black hover:bg-social-green">Twitter</button>
-                        <button href='#' className="text-center text-white hover:text-black hover:bg-social-green">Instagram</button>
-
+                <p className="text-white">"Front-end developer and avid reader."</p>
+                <div className="grid bg-[#1F1F1F]v">
+                        <button href='#' className="h-11 mt-3.5 bg-[#333333] text-center text-white hover:text-black hover:bg-social-green">GitHub</button>
+                        <button href='#' className="h-11 mt-3.5 bg-[#333333] text-center text-white hover:text-black hover:bg-social-green">Frontend Mentor</button>
+                        <button href='#' className="h-11 mt-3.5 bg-[#333333] text-center text-white hover:text-black hover:bg-social-green">LinkedIn</button>
+                        <button href='#' className="h-11 mt-3.5 bg-[#333333] text-center text-white hover:text-black hover:bg-social-green">Twitter</button>
+                        <button href='#' className="h-11 mt-3.5 bg-[#333333] text-center text-white hover:text-black hover:bg-social-green">Instagram</button>
                 </div>
             </div>
-            <div className="attribution text-center my-4">
+            <div className="attribution text-center my-4 text-white">
                 Challenge by
-                <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noreferrer'> Frontend Mentor</a>. Coded by<a href="mailto: JeffSokolDev@gmail.com">Jeff Sokol - JeffSokolDev@gmail.com</a>.
+                <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noreferrer'> Frontend Mentor</a>Coded by<a href="mailto: JeffSokolDev@gmail.com">Jeff Sokol - JeffSokolDev@gmail.com</a>
             </div>
         </div>
     )
